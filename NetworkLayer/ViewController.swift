8f338e511c74edd7448e839125dc449d6d71ea6a
Ali <alikjaber.91@gmail.com>
2022-07-31 07:50:09 +0300

    api call example

--- NetworkLayer/ViewController.swift
+++ NetworkLayer/ViewController.swift
@@ -8,10 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var networkManager =  HTTPClient()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    private func fireAPI() {
+        Task {
+            let loginResult = await networkManager.loginAPIExample()
+            switch loginResult {
+            case .success(let data):
+                print(data)
+                //DO whatever u want here
+            case .failure(let error):
+                print(error.customMessage)
+                //Display error message
+            }
+        }
     }
 
 
